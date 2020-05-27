1a6e4fe400727f9efa69507bd8ed8739cfaa7443
wusuiwei <1732405580@qq.com>
2020-05-27 20:28:39 +0800

    日志框架集成

--- crowdfunding-admin-component/src/test/java/Test.java
+++ crowdfunding-admin-component/src/test/java/Test.java
@@ -1,6 +1,8 @@
 import com.wusuiwei.crowdfunding.entity.Admin;
 import com.wusuiwei.crowdfunding.mapper.AdminMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -10,6 +12,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 
+@Slf4j
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration("classpath:spring-persist-mybatis.xml")
 public class Test {
@@ -30,6 +33,6 @@ public class Test {
     @org.junit.jupiter.api.Test
     void testMapper() {
         Admin admin = adminMapper.selectByPrimaryKey(1);
-        System.out.println(admin);
+        log.debug(admin.toString());
     }
 }
